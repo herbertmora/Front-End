31ab06d7c939c0adf0f0119af087b1ae48bcfa05
marygarciad <57425535+marygarciad@users.noreply.github.com>
2020-04-24 18:57:10 -0500

    Create people.model.ts
    Se realiz la creación de la clase people con los respectivos parametros.

---
+++ src/app/Models/people.model.ts
@@ -0,0 +1,21 @@
+
+import {Deserializable} from "./deserializable.model";
+
+export class people {
+  name: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  birth_year: string;
+  gender: string;
+  homeworld: string;
+  films: string;
+  species: string;
+  vehicles: string;
+  starships: string;
+  created: string;
+  edited: string;
+  url: string;
+}
