0ba01f607d2721a7334585b85fe542bde79a31b3
marygarciad <57425535+marygarciad@users.noreply.github.com>
2020-04-24 18:59:04 -0500

    Create vehicle.model.ts
    Se realiz la creación de la clase vehicle con los respectivos parametros.

---
+++ src/app/Models/vehicle.model.ts
@@ -0,0 +1,16 @@
+
+import {Deserializable} from "./deserializable.model";
+
+export class vehicle {
+  name: string;
+  model: string;
+  manufacturer: string;
+  cost_in_credits: string;
+  length: string;
+  max_atmosphering_speed: string;
+  crew: string;
+  passengers: string;
+  cargo_capacity: string;
+  consumables: string;
+  vehicle_class: string;
+}
