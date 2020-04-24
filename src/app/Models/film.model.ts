9f35d259b621093a838c067c34c3edd504221039
marygarciad <57425535+marygarciad@users.noreply.github.com>
2020-04-24 18:56:08 -0500

    Create film.model.ts
    Se realiza la creación de la clase Film con los respectivos parametros.

---
+++ src/app/Models/film.model.ts
@@ -0,0 +1,11 @@
+
+import {Deserializable} from "./deserializable.model";
+
+export class film {
+  title: string;
+  episode_id: string;
+  opening_crawl: string;
+  director: string;
+  producer: string;
+  release_date: string;
+}
