707283f5bd4b84cfed9271412b5ed884bc1131c2
marygarciad <57425535+marygarciad@users.noreply.github.com>
2020-04-24 19:05:29 -0500

    Create people.service.ts
    Se realiza la creación del servicio people que consume su respectiva api.

---
+++ src/app/servicios/people.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { map, finalize } from 'rxjs/operators';
+
+
+@Injectable()
+export class PeopleService {
+
+  constructor(public httpClient: HttpClient) { }
+
+  public GetPeople() {  
+    debugger;
+  const url = 'https://swapi.py4e.com/api/people/1/';
+
+  const options = {
+    headers: new HttpHeaders(
+      {
+        'Content-Type': 'application/json'       
+      }
+    )
+  };
+  return this.httpClient.get(url, options).pipe(map((response: Response) => {  return response;  }) );
+
+  }
+
+}
