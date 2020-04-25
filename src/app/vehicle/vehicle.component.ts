1bd2e7799076adaa5fcc151cd0578ff79146e7c2
marygarciad <57425535+marygarciad@users.noreply.github.com>
2020-04-24 19:17:14 -0500

    Create vehicle.component.ts

---
+++ src/app/vehicle/vehicle.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { VehicleService } from '../servicios/vehicle.service';
+
+@Component({
+  selector: 'app-vehicle',
+  templateUrl: './vehicle.component.html',
+  styleUrls: ['./vehicle.component.scss']
+})
+export class VehicleComponent implements OnInit {
+
+  vehicle: any;
+
+  constructor(private VehicleService: VehicleService) {
+
+   }
+
+  ObtenerVehicle() {
+    debugger;
+    this.VehicleService.GetVehicle().subscribe( resultado => {
+       this.vehicle = resultado;
+       console.log(JSON.stringify(resultado));
+     },
+       error => {
+         console.log(JSON.stringify(error));
+       });   
+   }   
+
+   ngOnInit() {
+    debugger;
+    this.ObtenerVehicle();
+  } 
+
+
+}
