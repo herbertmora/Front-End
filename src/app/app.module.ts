87425b93476b379a5a4aaf29c30304c23c548967
marygarciad <57425535+marygarciad@users.noreply.github.com>
2020-04-24 19:23:09 -0500

    Update app.module.ts
    Se realiza la importación de cada servicio y componente para que se visualice en el Index.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,18 @@ import { FooterComponent } from './footer/footer.component';
 import { ContentsMainComponent } from './contentsMain/contentsmain.component';
 import { BodyFilmsComponent } from './body-films/body-films.component';
 
+import { FilmService} from './servicios/film.service';
+import { FilmComponent} from './film/film.component';
+import { PeopleService} from './servicios/people.service';
+import { PeopleComponent} from './people/people.component';
+import { PlanetService} from './servicios/planet.service';
+import { PlanetComponent} from './planet/planet.component';
+import { StarshipService} from './servicios/starship.service';
+import { StarshipComponent} from './starship/starship.component';
+import { VehicleService} from './servicios/vehicle.service';
+import { VehicleComponent} from './vehicle/vehicle.component';
+import { SpeciesService} from './servicios/species.service';
+import { SpeciesComponent} from './species/species.component';
 
 
 @NgModule({
@@ -18,13 +30,19 @@ import { BodyFilmsComponent } from './body-films/body-films.component';
     HeaderComponent,
     FooterComponent,
     ContentsMainComponent,
-    BodyFilmsComponent
+    BodyFilmsComponent,
+    FilmComponent,
+    PeopleComponent,
+    PlanetComponent,
+    StarshipComponent,
+    VehicleComponent,
+    SpeciesComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule
   ],
-  providers: [DataSWService],
+  providers: [DataSWService, FilmService, PeopleService, PlanetService, StarshipService, VehicleService, SpeciesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
