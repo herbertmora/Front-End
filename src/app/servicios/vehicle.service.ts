6a432066c90c3d7d0b6fb4497bb0c317f8dee656
marygarciad <57425535+marygarciad@users.noreply.github.com>
2020-04-24 19:07:22 -0500

    Create vehicle.service.ts
    Se realiza la creación del servicio vehicle que consume su respectiva api.

---
+++ src/app/servicios/vehicle.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { map, finalize } from 'rxjs/operators';
+
+
+@Injectable()
+export class VehicleService {
+
+  constructor(public httpClient: HttpClient) { }
+
+  public GetVehicle() {  
+    debugger;
+  const url = 'https://swapi.py4e.com/api/vehicles/14/';
+
+  const options = {
+    headers: new HttpHeaders(
+      {
+        'Content-Type': 'application/json'       
+      }
+    )
+  };
+  return this.httpClient.get(url, options).pipe(map((response: Response) => {  return response;  }) );
+
+  }
+
+}
