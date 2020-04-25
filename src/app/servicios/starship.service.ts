88b89765948f42a5d57c400850d0e021922630e0
marygarciad <57425535+marygarciad@users.noreply.github.com>
2020-04-24 19:06:54 -0500

    Create starship.service.ts
    Se realiza la creación del servicio starship que consume su respectiva api.

---
+++ src/app/servicios/starship.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { map, finalize } from 'rxjs/operators';
+
+
+@Injectable()
+export class StarshipService {
+
+  constructor(public httpClient: HttpClient) { }
+
+  public GetStarship() {  
+    debugger;
+  const url = 'https://swapi.py4e.com/api/starships/9/';
+
+  const options = {
+    headers: new HttpHeaders(
+      {
+        'Content-Type': 'application/json'       
+      }
+    )
+  };
+  return this.httpClient.get(url, options).pipe(map((response: Response) => {  return response;  }) );
+
+  }
+
+}
