cc3c8279f06c62aa917fc1ad2f93b8e14570a51f
marygarciad <57425535+marygarciad@users.noreply.github.com>
2020-04-24 19:16:41 -0500

    Create starship.component.ts
    Se crea componente starship que llama al servicio.

---
+++ src/app/starship/starship.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { StarshipService } from '../servicios/starship.service';
+
+@Component({
+  selector: 'app-starship',
+  templateUrl: './starship.component.html',
+  styleUrls: ['./starship.component.scss']
+})
+export class StarshipComponent implements OnInit {
+
+  starship: any;
+
+  constructor(private StarshipService: StarshipService) {
+
+   }
+
+  ObtenerStarship() {
+    debugger;
+    this.StarshipService.GetStarship().subscribe( resultado => {
+       this.starship = resultado;
+       console.log(JSON.stringify(resultado));
+     },
+       error => {
+         console.log(JSON.stringify(error));
+       });   
+   }   
+
+   ngOnInit() {
+    debugger;
+    this.ObtenerStarship();
+  } 
+
+
+}
