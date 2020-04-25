1d3646f21026c74a9157d3a60219060e7b440279
marygarciad <57425535+marygarciad@users.noreply.github.com>
2020-04-24 19:05:04 -0500

    Create film.service.ts
    Se realiza la creación del servicio film que consume su respectiva api.

---
+++ src/app/servicios/film.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { map, finalize } from 'rxjs/operators';
+
+
+@Injectable()
+export class FilmService {
+
+  constructor(public httpClient: HttpClient) { }
+
+  public GetFilm() {  
+    debugger;
+  const url = 'https://swapi.py4e.com/api/films/1/';
+
+  const options = {
+    headers: new HttpHeaders(
+      {
+        'Content-Type': 'application/json'       
+      }
+    )
+  };
+  return this.httpClient.get(url, options).pipe(map((response: Response) => {  return response;  }) );
+
+  }
+
+}
